ef1f8df0be5b71d88c6b9bd717786cb6ab02e658
DaniNovoa <92156308+DaniNovoa@users.noreply.github.com>
2021-10-08 12:56:11 +0200

    Add files via upload

--- libdni.py
+++ libdni.py
@@ -1,6 +1,4 @@
-def calculoletra(dni):
-  letra=('TRWAGMYFPDXBNJZSQVHLCKE')
-  return (letra[dni%23])
-print("Este programa calcula la letra de tu dni:")  
+print("Este programa calcula la letra de tu dni:")
 dni=int(input("Introduzca su DNI:"))
-print("Su DNI completo es: {}{}.".format(dni,calculoletra(dni)))
+Letra=('TRWAGMYFPDXBNJZSQVHLCKE')
+print('La letra que contiene su codigo de DNI es:', Letra[dni%23])
